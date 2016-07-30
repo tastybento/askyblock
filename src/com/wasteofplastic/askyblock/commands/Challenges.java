1ab757aaf2caed488a332096ad17e409c8dc0f56
tastybento <tastybento@wasteofplastic.com>
2016-07-30 12:08:10 -0700

    Ops are not seeing challenge reward commands execute for them because they have the exclude permission. Causes confusion, so fixed it.

--- src/com/wasteofplastic/askyblock/commands/Challenges.java
+++ src/com/wasteofplastic/askyblock/commands/Challenges.java
@@ -376,7 +376,7 @@ public class Challenges implements CommandExecutor, TabCompleter {
 
     private void runCommands(Player player, List<String> commands) {
         // Ignore commands with this perm
-        if (player.hasPermission(Settings.PERMPREFIX + "command.challengeexempt")) {
+        if (player.hasPermission(Settings.PERMPREFIX + "command.challengeexempt") && !player.isOp()) {
             return;
         }
         for (String cmd : commands) {
