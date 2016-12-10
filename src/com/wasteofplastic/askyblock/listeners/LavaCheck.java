7ed9e2b327f40da89870bcd3eea344cde3ad4ffb
tastybento <tastybento@wasteofplastic.com>
2016-12-10 10:13:14 -0800

    Removed import

--- src/com/wasteofplastic/askyblock/listeners/LavaCheck.java
+++ src/com/wasteofplastic/askyblock/listeners/LavaCheck.java
@@ -18,7 +18,6 @@ package com.wasteofplastic.askyblock.listeners;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
