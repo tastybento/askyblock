a32736a69b9d4b369f00fa14afe2ced7716f5911
Yiğit Duman <yigitq@gmail.com>
2018-05-09 14:38:57 +0300

    Update PluginConfig.java

--- src/com/wasteofplastic/askyblock/PluginConfig.java
+++ src/com/wasteofplastic/askyblock/PluginConfig.java
@@ -526,6 +526,7 @@ public class PluginConfig {
         Settings.limitedBlocks = new HashMap<String,Integer>();
         Settings.entityLimits = new HashMap<EntityType, Integer>();
         Settings.saveEntities = plugin.getConfig().getBoolean("general.saveentitylimits");
+        Settings.coopsCanCreateWarps = plugin.getConfig().getBoolean("general.coopscancreatewarps");
         plugin.getLogger().info("Loading entity limits");
         ConfigurationSection entityLimits = plugin.getConfig().getConfigurationSection("general.entitylimits");
         if (entityLimits != null) {
